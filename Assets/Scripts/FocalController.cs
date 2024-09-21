7ac30e3c202bf44c2d0ffbcf56e902c5378432d4
Nick-Beam <Nicolas@beamimpact.com>
2024-09-21 13:54:07 -0400

    Added a basic player controller

--- Assets/Scripts/FocalController.cs
+++ Assets/Scripts/FocalController.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class FocalController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private float rotationSpeed = 200f;
+    private GameObject player;
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+     transform.position = player.transform.position;   
     }
 }
