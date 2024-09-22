9e71a64a4cd69db8ec066a74d4a3f04616ff5449
Nicolas Ries <nries1@gmail.com>
2024-09-21 21:09:56 -0400

    Add a basic player controller (#6)
    * Add player sphere
    
    * Added a basic player controller
    
    ---------
    
    Co-authored-by: Nick-Beam <Nicolas@beamimpact.com>

---
+++ Assets/Scripts/FocalController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocalController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float rotationSpeed = 200f;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+     transform.position = player.transform.position;   
+    }
+}
