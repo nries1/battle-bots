e2044e1fbe1bd187fd5132b6280e6418bc0d0b92
Nicolas Ries <nries1@gmail.com>
2024-09-22 21:21:58 -0400

    Add the latest arena to the scene (#12)
    * import scene
    
    * import new arena
    
    * add box colliders to saws
    
    * add network manager
    
    * create additional scene
    
    * standardize box colider size
    
    ---------
    
    Co-authored-by: Nick-Beam <Nicolas@beamimpact.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    void Awake()
+    {
+        transform.position = new Vector3(-55.3f, 2.3f, -52.4f);
+    }
     // Start is called before the first frame update
     void Start()
     {
