d1335beec481af66c585a83b7c4fe27a74b4bf0a
Nick-Beam <Nicolas@beamimpact.com>
2024-10-01 00:03:03 -0400

    prep for simple

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-    private Rigidbody playerRb;
+    private Rigidbody sphereRb;
     private GameObject focalPoint;
+    private GameObject sphere;
+
     void Awake()
     {
-        transform.position = new Vector3(-13f, 2.3f, -93f);
+        // transform.position = new Vector3(-13f, 2.3f, -93f);
     }
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        sphere = GameObject.Find("Sphere");
+        sphereRb = sphere.GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        sphereRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        // transform.position = sphere.transform.position;
     }
 }
