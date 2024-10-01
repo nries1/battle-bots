481f3f41110deea92544bd520337d483039278c0
Nick-Beam <Nicolas@beamimpact.com>
2024-10-01 13:57:43 -0400

    restart coroutine when powerup is picked up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,97 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement; // For loading the game over scene
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] public int maxHealth = 100; // Maximum health the player can have
     [SerializeField] private float speed = 5.0f;
-    private Rigidbody sphereRb;
-    private GameObject focalPoint;
-    private GameObject sphere;
+    public int currentHealth;
+    [SerializeField] TextMeshPro healthBarComponent;
+    private Rigidbody playerRb;
+    [SerializeField] private GameObject focalPoint;
+    [SerializeField] private SpawnManager spawnManager;
+    private bool hasPowerUp = false;
+    private Renderer componentRenderer;
+    private Color initialColor;
 
-    void Awake()
-    {
-        // transform.position = new Vector3(-13f, 2.3f, -93f);
-    }
-    // Start is called before the first frame update
+    private Coroutine activePowerupCountdown;
+
     void Start()
     {
-        sphere = GameObject.Find("Sphere");
-        sphereRb = sphere.GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
+        currentHealth = maxHealth; // Initialize current health to max health
+        componentRenderer = GetComponent<Renderer>();
+        initialColor = componentRenderer.material.color;
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    // Method to add HP
+    public void AddHealth(int amount)
     {
+        currentHealth += amount;
+        if (currentHealth > maxHealth) // Prevent exceeding max health
+        {
+            currentHealth = maxHealth;
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        HandleDamageCollision(other);
+        HandlePowerupCollision(other);
+    }
+
+    void HandleDamageCollision(Collider other) {
+        if (!other.gameObject.CompareTag("DamageBox")) return;
+        GameObject otherGameObject = other.GameObject();
+        int damageDealt = otherGameObject.GetComponent<DamageComponent>().damageDealt;
+        TakeDamage(damageDealt);
+    }
+    void HandlePowerupCollision(Collider other) {
+        if (!other.gameObject.CompareTag("PowerUp")) return;
+        if (!activePowerupCountdown.IsUnityNull()) {
+            StopCoroutine(activePowerupCountdown);
+        }
+        activePowerupCountdown = StartCoroutine(PowerUpCountdown());
+    }
+
+    IEnumerator PowerUpCountdown() {
+        hasPowerUp = true;
+        yield return new WaitForSeconds(spawnManager.powerUpDuration);
+        hasPowerUp = false;
+    }
+    // Method to take damage
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0) // Check if health reaches 0 or below
+        {
+            currentHealth = 0;
+            Debug.Log("Player has died!");
+            GameOver(); // Call game over method
+            return;
+        }
+        float ratioHealth = (float)currentHealth / maxHealth;
+        healthBarComponent.text = Mathf.RoundToInt(ratioHealth * 100) + "%";
+        Debug.Log("Current Health: " + currentHealth);
+    }
+
+    // Game over method
+    private void GameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    void FixedUpdate()
+    {
+        if (hasPowerUp) {
+            componentRenderer.material.color = Color.green;
+        } else {
+            componentRenderer.material.color = initialColor;
+        }
         float forwardInput = Input.GetAxis("Vertical");
-        sphereRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
-        // transform.position = sphere.transform.position;
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
